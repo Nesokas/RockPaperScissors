1a59e6000444467e67247cd9dd200e5339be2287
Nesokas <ines.moital@gmail.com>
2014-10-21 11:42:44 +0100

    Started rooms

---
+++ Assets/Rooms.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook;
+
+public class Rooms : MonoBehaviour {
+
+	private Room[] rooms;
+	private Vector2 scrollbar_position;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect(Screen.width*0.1f, Screen.height*0.25f, Screen.width*0.8f, Screen.height*0.6f), "");
+		GUILayout.BeginArea(new Rect(Screen.width*0.1f, Screen.height*0.25f, Screen.width*0.8f, Screen.height*0.6f));
+		scrollbar_position = GUILayout.BeginScrollView(scrollbar_position, false, true, GUILayout.Width(Screen.width*0.8f), GUILayout.Height(Screen.height*0.6f));
+
+		foreach(RoomInfo room in PhotonNetwork.GetRoomList()) {
+			GUILayout.Box(room.name);
+			if(GUILayout.Button("Join"))
+				PhotonNetwork.JoinRoom(room.name);
+		}
+
+		GUILayout.EndScrollView();
+		GUILayout.EndArea();
+	}
+
+	public void JoinRandomRoom()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	public void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom(FB.UserId);
+	}
+
+	public void CreateRoom()
+	{
+		PhotonNetwork.CreateRoom(FB.UserId, true, true, 2);
+	}
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Facebook;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () { 
+	public GameObject facebook_login;
+	public GameObject lobby;
+
+	public GameObject debug_text_object;
+	private Text debug_text;
+
+	void Awake()
+	{
+		if(!FB.IsInitialized){
+			FB.Init(SetInit);
+		} else {
+			SetInit();
+		}
+
+		debug_text = (Text)debug_text_object.GetComponent("Text");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void SetInit()
+	{
+		enabled = true;
+		if (FB.IsLoggedIn)
+		{
+			Debug.Log("SetInit()");
+			OnLoggedIn();
+		}
+	}
+
+	void Update()
+	{
+		debug_text.text = PhotonNetwork.connectionStateDetailed.ToString();
+		debug_text.text = "Update";
+		if(PhotonNetwork.connectionStateDetailed == PeerState.JoinedLobby){
+			Debug.Log("joined lobby");
+			facebook_login.SetActive(false);
+			lobby.SetActive(true);
+		}
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		Debug.Log("OnHideUnity()");
+	} 
+
+	void LoginCallback(FBResult result)
+	{
+		if (FB.IsLoggedIn)
+		{
+			OnLoggedIn();
+		}
 	}
 
 	public void StartOfflineGame()
 	{
 		Application.LoadLevel(1);
 	}
+
+	public void LoginFacebook()
+	{
+		Debug.Log("Button Pressed");
+		FB.Login("email", LoginCallback);
+	}
+
+	void OnLoggedIn()
+	{
+		PhotonNetwork.AuthValues = new AuthenticationValues();
+		PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.Facebook;
+		PhotonNetwork.AuthValues.SetAuthParameters(FB.UserId, FB.AccessToken);
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
 }
+
+
